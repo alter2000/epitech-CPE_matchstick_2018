77bbcd30f1957544e033269ae6a9e3809d4791e9
Jor Bashllari <altnate2000@gmail.com>
2019-01-27 18:29:56 +0100

    convert to EOF for stdin

--- events.c
+++ events.c
@@ -11,14 +11,15 @@ static int get_user_line(board_t *b, int *ln)
 {
     char *buf;
 
-    my_puts("Line: ");
+    my_putstr("\nLine: ");
     buf = getl(1);
     if (!buf)
         return -1;
     *ln = my_atoi((const char **)&buf);
-    if (*ln < 1 && my_puts("Error: invalid input (positive number expected)"))
+    if (*ln < 1 && my_putstr("\nError: invalid input "
+                "(positive number expected)"))
         return 1;
-    if ((*ln > b->len) && my_puts("Error: this line is out of range"))
+    if ((*ln > b->len) && my_putstr("\nError: this line is out of range"))
         return 1;
     return 0;
 }
@@ -34,20 +35,22 @@ static int get_user_match(board_t *b, int *m, int *ln)
 {
     char *buf;
 
-    my_puts("Matches: ");
+    my_putstr("\nMatches: ");
     buf = getl(1);
     if (!buf)
         return -1;
-    if (*buf == '0' && my_puts("Error: you have to remove at least one match"))
+    if (*buf == '0' && my_putstr("\nError: you have to remove at "
+                "least one match"))
         return 1;
     *m = my_atoi((const char **)&buf);
-    if (*m <= 0 && my_puts("Error: invalid input (positive number expected)"))
+    if (*m <= 0 && my_putstr("\nError: invalid input "
+                "(positive number expected)"))
         return 1;
-    if (*m > b->stnum && my_printf("Error: you cannot remove"
-                " more than %d matches per turn\n", b->stnum))
+    if (*m > b->stnum && my_printf("\nError: you cannot remove"
+                " more than %d matches per turn", b->stnum))
         return 1;
     if (!enough_matches(b, *ln, *m) && \
-            my_puts("Error: not enough matches on this line"))
+            my_putstr("\nError: not enough matches on this line"))
         return 1;
     return 0;
 }
@@ -56,7 +59,7 @@ static int get_user(board_t *board, int *ln, int *mat)
 {
     int ret = 0;
 
-    my_puts("Your turn:");
+    my_putstr("Your turn:");
     while (1) {
         ret = get_user_line(board, ln);
         if (1 == ret)
@@ -80,7 +83,7 @@ int events(board_t *board, bool *user)
     int match;
 
     if (*user) {
-        if (get_user(board, &line, &match) && my_printf("Player removed %d "
+        if (get_user(board, &line, &match) && my_printf("\nPlayer removed %d "
                     "match(es) from line %d\n", match, line)) {
             (*user) = !(*user);
             return mod_board(board, line, match);
